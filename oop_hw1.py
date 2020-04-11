494a99caebe079140e073548deb7aaa4224fdf9b
Fatime Ramazanova <fatimeramazanova2000@gmail.com>
2020-04-11 20:17:00 +0300

    Task1
    4) Створити клас, що характеризує книгу: автор, назва,
    видавництво, рік, кількість сторінок.

---
+++ oop_hw1.py
@@ -0,0 +1,19 @@
+class Book():
+    author = ""
+    name = ""
+    year = ""
+    publishing = ""
+    pages = ""
+
+newBook = Book()
+newBook.author = "Author: Veronica Roth"
+newBook.name = "Book name: Divergent"
+newBook.year = "Published: 2011"
+newBook.publishing = "Publishing house: Katherine Tegen Books"
+newBook.pages = "Number of pages: 496"
+
+print(newBook.author)
+print(newBook.name)
+print(newBook.year)
+print(newBook.publishing)
+print(newBook.pages)
